f0ccb15b6c1828e323740f233c2b98c1864a5462
mirelamoser <mirelamoser@gmail.com>
2026-07-07 16:41:46 -0300

    Removendo arquivos do IFSC

--- projeto-integrador-II/PhiloQuest-260528/autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-/**
- * Compatibilidade com entradas legadas que ainda referenciam autoload.php.
- * O carregamento oficial é via Composer (vendor/autoload.php) em bootstrap.php.
- */
-require_once __DIR__ . '/vendor/autoload.php';
